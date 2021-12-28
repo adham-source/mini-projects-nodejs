72aef9b35bb17df89eb96a0f21f7b436231501c4
adham-source <adham.ahmad1988as@gmail.com>
2021-12-29 00:09:05 +0200

    Create model User and validate

---
+++ auth-system/src/constants/index.js
@@ -0,0 +1,8 @@
+import { config } from 'dotenv'
+config()
+
+export const DB = process.env.BATABASE_URI
+export const SECRET = process.env.APP_SECRET
+export const DOMIN = process.env.APP_DOMIN
+export const SENDGRID = process.env.SENDGRID_API
+export const PORT = process.env.APP_PORT
--- auth-system/src/index.js
+++ auth-system/src/index.js
@@ -1,1 +1,35 @@
-console.log('Hello world')
+// import cors from "cors"
+import consola from "consola"
+import express, { application } from "express"
+import mongoose from "mongoose"
+import { json } from "body-parser"
+
+// Import application constants
+import { DB, PORT } from "./constants"
+
+// Router exports
+import userRoutes from './routes/users'
+
+// Initialize express application
+const app = express()
+
+// Apply application middlewares
+// app.use(cors())
+app.use(json())
+
+// Inject sub router 
+app.use('/users', userRoutes)
+
+const main = async () => {
+    try {
+        // Connect with the database
+        await mongoose.connect(DB, {
+            useNewUrlParser: true
+        })
+        consola.success("Database connected ..")
+        app.listen(PORT, () => consola.success(`Server started on port ${PORT}`))
+    } catch(err) {
+        consola.error(`Unable to start the server \n${err.message}`)
+    }
+}
+main()
